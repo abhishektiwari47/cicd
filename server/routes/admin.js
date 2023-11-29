b95a583dd58475dd0ac33890e32835aa94f35d36
Abhishek <abhitiwari3010@gmail.com>
2023-11-29 21:33:37 +0530

    kdfjkh

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -19,7 +19,7 @@ router.get("/me", authenticateJwt, async (req, res) => {
 });
 
 router.get("/some",async (req,res)=>{
-  res.json({"message":"This is so greatdfd..lund"});
+  res.json({"message":"This is so greatdfd..dfdflund"});
 })
 
 router.post('/signup', (req, res) => {
