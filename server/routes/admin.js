9c52d726673ee64ff715047936f0343ae92277e9
Abhishek <abhitiwari3010@gmail.com>
2023-11-29 19:05:38 +0530

    added

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -19,7 +19,7 @@ router.get("/me", authenticateJwt, async (req, res) => {
 });
 
 router.get("/some",async (req,res)=>{
-  res.json({"message":"This is so great.."});
+  res.json({"message":"This is so great..jj"});
 })
 
 router.post('/signup', (req, res) => {
